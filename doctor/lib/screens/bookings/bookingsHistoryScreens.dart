b17e79166188e77823e1f1816f3edf8dfc8d8983
parivesh1 <parivesh.1210@gmail.com>
2022-01-05 15:45:13 +0530

    _

---
+++ doctor/lib/screens/bookings/bookingsHistoryScreens.dart
@@ -0,0 +1,401 @@
+import 'package:flutter/material.dart';
+
+class BookingsHistoryScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Row(
+          children: [
+            Container(
+              height: MediaQuery.of(context).size.height * 0.21,
+              width: MediaQuery.of(context).size.width,
+              padding: EdgeInsets.all(20),
+              decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 158, 129, 190),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: const Radius.circular(40),
+                    bottomRight: const Radius.circular(40),
+                  ),
+                  gradient: LinearGradient(
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                    colors: [
+                      Color.fromARGB(255, 92, 204, 255),
+                      Color.fromARGB(255, 158, 129, 190)
+                    ],
+                  )),
+              child: Column(
+                children: [
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        "Appointments",
+                        style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w400,
+                            color: Colors.white),
+                      )
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.start,
+                    children: [
+                      Padding(
+                        padding:
+                            const EdgeInsets.only(left: 1, top: 8, bottom: 10),
+                        child: Text(
+                          "June",
+                          style: TextStyle(
+                              fontSize: 20,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.white),
+                        ),
+                      ),
+                    ],
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Column(
+                        children: [
+                          Text(
+                            "S",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "6",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "M",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          CircleAvatar(
+                            child: Text(
+                              "7",
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.w400,
+                                  color: Color.fromARGB(255, 158, 129, 190)),
+                            ),
+                            radius: 13,
+                            backgroundColor: Colors.white,
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "T",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "8",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "W",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "9",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "T",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "10",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "F",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "11",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                      Column(
+                        children: [
+                          Text(
+                            "S",
+                            style: TextStyle(
+                                fontSize: 10,
+                                fontWeight: FontWeight.w300,
+                                color: Colors.white),
+                          ),
+                          Text(
+                            "12",
+                            style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.w400,
+                                color: Colors.white),
+                          )
+                        ],
+                      ),
+                    ],
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(
+                        top: 19, left: 13.5, right: 13.5, bottom: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(""),
+                        Text(
+                          "Monday  June 7, 2021",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 15,
+                              fontWeight: FontWeight.w600),
+                        ),
+                        Icon(
+                          Icons.calendar_today_rounded,
+                          color: Colors.white,
+                          size: 15,
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+        HistoryCards(),
+        HistoryCards(),
+        HistoryCards(),
+        HistoryCards(),
+        HistoryCards(),
+      ],
+    );
+  }
+}
+
+class HistoryCards extends StatelessWidget {
+  const HistoryCards({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        showBottomSheet(context);
+      },
+      child: Card(
+        elevation: 12,
+        shadowColor: Colors.grey,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                  Text(
+                    "Samir Kumar",
+                    style: TextStyle(fontSize: 18, fontWeight: FontWeight.w700),
+                  ),
+                  Text(
+                    "10:00 AM",
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.w300),
+                  ),
+                  SizedBox(
+                    height: MediaQuery.of(context).size.height * 0.01,
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.only(right: 8),
+              child: CircleAvatar(
+                child: CircleAvatar(
+                  child: Text(
+                    "1",
+                    style: TextStyle(color: Colors.black),
+                  ),
+                  radius: 16,
+                  backgroundColor: Colors.white,
+                ),
+                radius: 16.5,
+                backgroundColor: Colors.black,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  Future<dynamic> showBottomSheet(BuildContext context) {
+    return showModalBottomSheet(
+        context: context,
+        builder: (BuildContext context) {
+          return Container(
+            height: MediaQuery.of(context).size.height * 0.17,
+            child: Container(
+              decoration: BoxDecoration(
+                color: Color.fromARGB(255, 158, 129, 190),
+                borderRadius: BorderRadius.only(
+                  topLeft: const Radius.circular(35),
+                  topRight: const Radius.circular(35),
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(12),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListTile(
+                      leading: Icon(
+                        Icons.person,
+                        size: 30,
+                        color: Colors.white,
+                      ),
+                      title: Text("Patient Details",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontSize: 19,
+                              fontWeight: FontWeight.w700)),
+                      trailing: Container(
+                        child: Padding(
+                          padding: const EdgeInsets.all(9),
+                          child: Text(
+                            "Contact",
+                            style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 14,
+                                fontWeight: FontWeight.w700),
+                          ),
+                        ),
+                        decoration: BoxDecoration(
+                            color: Color.fromARGB(255, 158, 129, 190),
+                            border: Border.all(
+                              color: Colors.white,
+                              width: 1,
+                            ),
+                            borderRadius: BorderRadius.all(Radius.circular(5))),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 20),
+                      child:
+                          Divider(height: 1, thickness: 1, color: Colors.white),
+                    ),
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height * 0.014,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.symmetric(horizontal: 30),
+                      child: Column(
+                        children: [
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Name:",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14)),
+                              Text("Udhav Bajaj",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 14))
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Age:",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14)),
+                              Text("21",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 14))
+                            ],
+                          ),
+                          Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: [
+                              Text("Ph. Number:",
+                                  style: TextStyle(
+                                      color: Colors.white,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 14)),
+                              Text("9711462663",
+                                  style: TextStyle(
+                                      color: Colors.white, fontSize: 14))
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
+  }
+}
